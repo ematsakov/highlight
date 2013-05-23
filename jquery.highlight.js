c97ab683840f518622c2dcbdd275c8c890ae8660
ondrovic <ondrovic@gmail.com>
2013-05-23 05:30:46 -0300

    Update jquery.highlight.js
    added SQL highlighting

--- jquery.highlight.js
+++ jquery.highlight.js
@@ -264,6 +264,59 @@
 				.replace(/(&lt;|<)!--([\s\S]*?)--(&gt;|>)/gm,'<span class="com">$1!--$2--$3</span>');
 				
 			return code;
+		};
+		//SQL
+		hightlight_sql: function(code) {
+			var comments		= [];	// store comments
+			
+			var funcs	=	'abs avg case cast coalesce convert count current_timestamp ' +
+						'current_user day isnull left lower month nullif replace right ' +
+						'session_user space substring sum system_user upper user year';
+	
+			var keywords =	'absolute action add after alter as asc at authorization begin bigint ' +
+						'binary bit by cascade char character check checkpoint close collate ' +
+						'column commit committed connect connection constraint contains continue ' +
+						'create cube current current_date current_time cursor database date ' +
+						'deallocate dec decimal declare default delete desc distinct double drop ' +
+						'dynamic else end end-exec escape except exec execute false fetch first ' +
+						'float for force foreign forward free from full function global goto grant ' +
+						'group grouping having hour ignore index inner insensitive insert instead ' +
+						'int integer intersect into is isolation key last level load local max min ' +
+						'minute modify move name national nchar next no numeric of off on only ' +
+						'open option order out output partial password precision prepare primary ' +
+						'prior privileges procedure public read real references relative repeatable ' +
+						'restrict return returns revoke rollback rollup rows rule schema scroll ' +
+						'second section select sequence serializable set size smallint static ' +
+						'statistics table temp temporary then time timestamp to top transaction ' +
+						'translation trigger true truncate uncommitted union unique update values ' +
+						'varchar varying view when where with work';
+			
+			var op =	'all and any between cross in join like not null or outer some';
+	    	
+	    	funcs = new RegExp(get_keywords(funcs), 'gi');
+			keywords = new RegExp(get_keywords(keywords), 'gi');
+			op = new RegExp(get_keywords(op), 'gi');
+
+			code = code
+				//replace strings
+				.replace(/(".*?")/g,'<span class="str">$1</span>')
+				.replace(/('.*?')/g,'<span class="str">$1</span>')	
+				//replace multiline comments
+				.replace(/\/\*([\s\S]*?)\*\//g, function(m, t)
+					{ return '\0C'+push(comments, multiline_comments(m))+'\0'; })
+				.replace(/\0C(\d+)\0/g, function(m, i)
+					{ return comments[i]; })
+				//replace one line comments
+				.replace(/\/\/(.*$)/gm,'<span class="com">//$1</span>')
+				//replace variables
+				.replace(/\$(\w+)/g,'<span class="var">$$$1</span>')
+				//replace functions
+				.replace(funcs,'<span class="fnc">$1</span>$2')
+				//replace keywords
+				.replace(keywords,'<span class="kwd">$1</span>$2')
+				//replace operators
+				.replace(op,'<span class="op">$1</span>$2');
+			return code;
 		}
 	};
 	
