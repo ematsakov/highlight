564c073f2d2ce1f5ff9766f930de9e66bf926e58
owen beresford <owenberesford@fastmail.fm>
2015-02-11 18:14:43 +0000

    More keywords from /usr/share/vim/vim74/syntax/php.vim (there are many more). Add # comments.

--- jquery.highlight.js
+++ jquery.highlight.js
@@ -170,11 +170,11 @@
 				'strtoupper strtr strval substr substr_compare';
 	
 			var keywords =	'and or xor array as break case ' +
-				'cfunction const continue declare default die do else ' +
-				'elseif enddeclare endfor endforeach endif endswitch endwhile ' +
-				'extends for foreach function include include_once global if ' +
+				'function const continue declare default die do else ' +
+				'elseif enddeclare endfor endforeach endif endswitch endwhile ' 
+				'for foreach function include include_once global if ' +
 				'new old_function return static switch use require require_once ' +
-				'while abstract interface public implements extends private protected throw';
+				'while abstract interface public implements extends private protected throw  __LINE__ __FILE__ __FUNCTION__ __METHOD__ __CLASS__ __DIR__ __NAMESPACE__  namespace instanceof parent self real bool double float string object null const var clone new true false';
 	
 			funcs = new RegExp(get_keywords(funcs), 'gi');
 			keywords = new RegExp(get_keywords(keywords), 'gi');
@@ -189,7 +189,8 @@
 				.replace(/\0C(\d+)\0/g, function(m, i)
 					{ return comments[i]; })
 				//replace one line comments
-				.replace(/\/\/(.*$)/gm,'<span class="com">//$1</span>')
+				.replace(/\/\/(.*$)/g,'<span class="com">//$1</span>')
+				.replace(/#(.*$)/g,'<span class="com">//$1</span>')
 				//replace variables
 				.replace(/\$(\w+)/g,'<span class="var">$$$1</span>')
 				//replace functions
